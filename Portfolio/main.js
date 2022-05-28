4bb296431e9c2ee7682799713cc55bd27aae37ea
GylleadhePT-BR <62726058+GylleadhePT-BR@users.noreply.github.com>
2022-05-28 15:12:13 -0300

    upgrade2

---
+++ Portfolio/main.js
@@ -0,0 +1,70 @@
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Gylleadhe", "a Web developer", "UX desinger", " the best for your project"];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  }
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  }
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+var i = 0;
+var A = 0;
+var tag = document.getElementById("about");
+var html = document.getElementById("about").innerHTML;
+var attr = tag.setAttribute("data", html);
+var txt = tag.getAttribute("data");
+var speed = 120;
+
+function typeWriter() {
+  if (i <= txt.length) {
+    document.getElementById("about").innerHTML = txt.slice(0, i + 1);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  console.log(document.getElementById("about").innerHTML);
+}
+
+typeWriter();
+
+
+function deltxt() {
+  if (A <= txt.length) {
+    document.getElementById("about").innerHTML = txt.slice(0, -A);
+    A++;
+    setTimeout(deltxt, 10);
+  }
+}
